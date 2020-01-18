f884b09810b0c3f5caa8f051658c1d1ca5f289e0
Argadeva <58824621+argadeva@users.noreply.github.com>
2020-01-18 19:02:23 +0700

    Hasil Akhir

---
+++ 1.js
@@ -0,0 +1,29 @@
+function bioData(myName, myAge) {
+    let data = {
+        name: myName,
+        age: myAge,
+        address: "Jepara, Jawa Tengah",
+        hobbies: ['Swimming', 'Cycling', 'Jogging'],
+        is_married: false,
+        list_school: [
+            { "name":"SD NEGERI LANGON 1 JEPARA", "year_in": 2001, "year_out": 2007, "major":"Elementary School" },
+            { "name":"SMP NEGERI 6 JEPARA", "year_in": 2007, "year_out": 2010, "major":"Junior High School" },
+            { "name":"SMK ISLAM JEPARA", "year_in": 2010, "year_out": 2013, "major":"Senior High School" },
+        ],
+        skills: [
+            { "skill_name":"HTML", "level":"EXPERT" },
+            { "skill_name":"PHP", "level":"ADVANCE" },
+            { "skill_name":"CSS", "level":"ADVANCE" },
+            { "skill_name":"BOOTSTRAP", "level":"ADVANCE" },
+            { "skill_name":"JAVASCRIPT", "level":"BEGINNER" },
+            { "skill_name":"NODE JS", "level":"BEGINNER" },
+            { "skill_name":"REACT JS", "level":"BEGINNER" },
+            { "skill_name":"MONGO DB", "level":"BEGINNER" },
+            { "skill_name":"FLUTTER", "level":"BEGINNER" },
+        ],
+        interest_in_coding: true
+    }
+    console.log(data);  
+}
+
+bioData("Alam Raga Deva", 24);
